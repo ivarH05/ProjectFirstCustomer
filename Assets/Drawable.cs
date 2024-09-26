2e695ff7f36054cf227a6e99e934238aa1bffe89
ivarH05 <119761956+ivarH05@users.noreply.github.com>
2024-09-26 15:02:37 +0200

    more fixes and journal

---
+++ Assets/Drawable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Drawable : Interactable
+{
+    public int PageID;
+
+    private void Start()
+    {
+        closeColor = new Vector3(1, 5, 5);
+        hoverColor = new Vector3(1, 5, 2);
+    }
+}
